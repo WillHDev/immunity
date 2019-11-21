bf20ed60aea71b1a98c0bad7d0f80b102113e31f
WillHDev <william.holcombe@uconn.edu>
2019-11-21 16:18:54 -0700

    finished with links and slugs

---
+++ pages/event/index.js
@@ -0,0 +1,16 @@
+import Layout from '../../components/Layout';
+
+
+
+const Event = () => {
+    
+    
+    return (
+        <Layout>
+            <h1>Event</h1>
+            
+        </Layout>
+    );
+};
+
+export default Event;
